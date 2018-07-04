34e4fe38a6b215118978a612b76d1bafdaa7f11e
ruioana <36535856+ruioana@users.noreply.github.com>
2018-07-04 13:46:19 +0100

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,34 @@
+"""program will ask the user to select between rock paper scissors. tell the computer to select too
+compare both choices and determine the winner based on the choices. Display the winner"""
+
+from random import randint
+options = ["ROCK", "PAPER", "SCISSORS"]
+message = {
+  "tie" : "Yawn, its a tie",
+  "won" : "Yay you won",
+  "lost" : "Aww you lost!"
+}
+
+def decide_winner(user_choice, computer_choice):
+  print ("You selected %s" % user_choice)
+  print ("Computer selected %s" % computer_choice)
+  if user_choice == computer_choice:
+    print (message["tie"])
+  elif user_choice == options[0] and computer_choice == options[2]:
+    print (message["won"])
+  elif  user_choice == options[1] and computer_choice == options[0]:
+    print (message["won"])
+  elif user_choice == options[2] and computer_choice == options[1]:
+    print (message["won"])
+  else:
+    print (message["lost"])
+ 
+def play_RPS():
+  print ("Rock, Paper or Scissors?")
+  user_choice = input("Enter Rock, Paper or Scissors: ")
+  user_choice = user_choice.upper()
+  computer_choice = options[randint(0,2)]
+  decide_winner(user_choice, computer_choice)
+  
+play_RPS()
+  
